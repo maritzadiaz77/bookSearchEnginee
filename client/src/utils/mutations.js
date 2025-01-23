b3f8304c8578c433d4e9d589738328a8d745512a
Maritza Diaz <maritzadiaz77@gmail.com>
2025-01-23 15:36:44 -0800

    Added mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,77 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        bookCount
+        email
+        savedBooks {
+          authors
+          bookId
+          description
+          image
+        }
+        username
+      }
+    }
+  }
+`;
+export const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        bookCount
+        email
+        username
+        savedBooks {
+          description
+          bookId
+          authors
+          image
+          title
+        }
+      }
+    }
+  }
+`;
+export const SAVEBOOK = gql`
+  mutation saveBook($bookInput: BookInput!) {
+    saveBook(bookInput: $bookInput) {
+      _id
+      bookCount
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+      username
+    }
+  }
+`;
+export const REMOVEBOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      bookCount
+      email
+      savedBooks {
+        authors
+        description
+        title
+        bookId
+        image
+        link
+      }
+      username
+    }
+  }
+`;
